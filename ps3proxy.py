a66438a91be7f4343869fe61b175c142d062440e
rrooij <rderooij685@gmail.com>
2018-07-01 16:37:22 +0200

    Fix US

--- ps3proxy.py
+++ ps3proxy.py
@@ -11,8 +11,5 @@ script_path = dirname(realpath(__file__))
 def response(flow: http.HTTPFlow) -> None:
     if flow.request.pretty_url == "http://feu01.ps3.update.playstation.net/update/ps3/list/eu/ps3-updatelist.txt":
         flow.response.content = open(script_path + '/eu.txt', 'rb').read()
-    elif flow.request.pretty_url == "http://feu01.ps3.update.playstation.net/update/ps3/list/us/ps3-updatelist.txt":
-        flow.response = http.HTTPResponse.make(
-            200,
-            open(script_path + '/us.txt', 'rb').read(),
-        )
+    elif flow.request.pretty_url == "http://fus01.ps3.update.playstation.net/update/ps3/list/us/ps3-updatelist.txt":
+        flow.response.content = open(script_path + '/us.txt', 'rb').read()
